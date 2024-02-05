918c0ab5c98bb6fc080022a38f8ebe05d196c92f
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2024-02-05 20:54:50 +0100

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCEDOTr_3W9oYSgT_nmWsATpXw8dAjggkY",
-    authDomain: "cato-server.firebaseapp.com",
-    projectId: "cato-server",
-    storageBucket: "cato-server.appspot.com",
-    messagingSenderId: "466353652050",
-    appId: "1:466353652050:web:9bcfe157f0d70a3a3ffa8b",
+
+  apiKey: "AIzaSyCEDOTr_3W9oYSgT_nmWsATpXw8dAjggkY",
+  authDomain: "cato-server.firebaseapp.com",
+  projectId: "cato-server",
+  storageBucket: "cato-server.appspot.com",
+  messagingSenderId: "466353652050",
+  appId: "1:466353652050:web:9bcfe157f0d70a3a3ffa8b",
 });
 
 const messaging = firebase.messaging();
@@ -15,12 +16,4 @@ const messaging = firebase.messaging();
 // Service Worker scope
 messaging.onBackgroundMessage((payload) => {
   console.log('Received background message:', payload);
-      // Customize notification here
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: payload.notification.icon,
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
 });
